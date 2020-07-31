696d624b20a09ecb4372f31e035d0ebf27217d66
Quinn94 <mayquinn94@gmail.com>
2020-07-31 22:34:19 +0800

    http

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const Person = require("./person");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const person1 = new Person("Quinn", 26);
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "./public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "./public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+});
 
-person1.greeting();
+//using nodemon, amending package.json scripts 'start' & 'dev'
+//cmd+opt+u : view source type
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- logger.js
+++ logger.js
@@ -1,4 +1,19 @@
 const EventEmitter = require("events");
 const uuid = require("uuid");
 
-console.log(uuid.v4());
+class Logger extends EventEmitter {
+  log(msg) {
+    //Call event
+    this.emit("message", { id: uuid.v4(), msg });
+  }
+}
+
+// module.exports = Logger;
+
+const logger = new Logger();
+
+logger.on("message", (data) => console.log(`Called Listener`, data));
+
+//calling the constructor
+logger.log("Hello World");
+logger.log("Hello");
